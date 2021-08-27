3a2aaea17c79c1f5b319f78aec6a0eac216acc6c
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-27 02:24:22 +0100

    smile-2

--- src/App.js
+++ src/App.js
@@ -5,14 +5,44 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Wekome to de liar
-        </p>
+        <div className="App">
+            {/* <!-- Header --> */}
+          <header class="main-header">
+
+            {/* <!-- title --> */}
+            <h2 id="logo">de_gentleman</h2>
+
+            {/* <!-- nav --> */}
+            <nav class="main-navbar">
+              <ul>
+                <li><a href="./index.html">Home</a></li>
+                <li><a href="./about.html">About</a></li>
+              </ul>
+            </nav>
+
+            {/* <!-- Button to contact page --> */}
+
+            <a href="./contact.html" target="_blank" class="btn"> 
+              Contact Me?
+            </a>
+
+          </header>
+
+          {/* <!-- main APP / CONTENT --> */}
+          <main>
+
+          {/* <!-- Ask User what is their name and favorite color --> */}
+          
+          {/* <!-- Say Hello User --> */}
+
+          {/* <!-- intro --> */}
+          <div class="intro">
+            <h2>Welcome to my</h2>
+            <h1>cyber junzoon</h1>
+            <h3>Major updates are too come.</h3>
+          </div>
+
+        </main>
       </div>
     );
   }
