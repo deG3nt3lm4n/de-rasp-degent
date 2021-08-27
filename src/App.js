fbcecc4cc1940a7c5b6eaaaf1899fa8dd2e8c60d
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-27 02:19:12 +0100

    smile

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          Hi i made it here lol
+          Wekome to de liar
         </p>
       </div>
     );
